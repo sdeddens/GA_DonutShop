86392b9b025c4d34e99a567e11803c2801047a77
Stephen Deddens <sdeddens@gmail.com>
2015-10-09 13:38:11 -0500

    list printing and click handler is working

--- ga_donut_shop/js/app.js
+++ ga_donut_shop/js/app.js
@@ -10,17 +10,17 @@ var myData = $.ajax({
     method: "GET",
 
     success: function(data, textStatus, jqXHR) {
-    console.log(data);
-    for (var i = 0; i < data.length; i++) {
-		    $( "#doughnuts" ).append("<li id="+data[i].id + ">"+ data[i].flavor + " | "+data[i].style+"</li>");
-    };
-
-
-
+	    console.log(data);
+	    for (var i = 0; i < data.length; i++) {
+			    $( "#doughnuts" ).append("<li id="+data[i].id + ">"+ data[i].flavor + " - "+data[i].style+"</li>");
+	    };
     }
 });
 
+$("#new-doughnut").click(function() {
 
+  alert( "Handler for .click() called." );
+});
 
 
 
