91b0e58616b0d024fba542ddc1de8631cbffa4d9
Stephen Deddens <sdeddens@gmail.com>
2015-10-09 12:44:39 -0500

    list of all doughnuts are on the screen

--- ga_donut_shop/js/app.js
+++ ga_donut_shop/js/app.js
@@ -1,4 +1,30 @@
 // jQuery Document Ready
 $(function() {
-  
+
+
+
+
+var myData = $.ajax({
+    url: "http://api.doughnuts.ga/doughnuts?",
+    dataType: "jsonp",
+    method: "GET",
+
+    success: function(data, textStatus, jqXHR) {
+    console.log(data);
+    for (var i = 0; i < data.length; i++) {
+		    $( "#doughnuts" ).append("<li id="+data[i].id + ">"+ data[i].flavor + " | "+data[i].style+"</li>");
+    };
+
+
+
+    }
+});
+
+
+
+
+
+
+
+
 });
